e4a8daaa7c95a3dea4dfdc97490fe2afcfea8dcb
BuschMath <busch@iowacentral.edu>
2024-09-09 09:26:52 -0500

    Add project files.

---
+++ Ch2_Example_FA24/Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// Function Prototype
+void PrintMessage();
+
+// Function Definition
+void PrintMessage()
+{
+	std::cout << "Hello world!\n";
+}
+
+int main()
+{
+	// Function call
+	PrintMessage();
+
+	return 0;
+}
